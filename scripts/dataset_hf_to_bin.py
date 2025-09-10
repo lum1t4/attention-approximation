4b990aef2a125b2c80cc176677f333685343b3b6
Gianluca Calò <gianluca.calo11@gmail.com>
2025-09-10 19:46:28 +0200

    dataset download with gated tokenizer

--- scripts/dataset_hf_to_bin.py
+++ scripts/dataset_hf_to_bin.py
@@ -34,13 +34,13 @@ def main(
         tokenizer: str,
         output_dir: str | Path,
         remote_name: str | None = None,
-        shard_size: int = 10**8 # 100M tokens per shard, total of 100 shards
+        shard_size: int = 10**8, # 100M tokens per shard, total of 100 shards
+        hf_token: str | None = None
     ):
     # Create output directory if it doesn't exist
     Path(output_dir).mkdir(parents=True, exist_ok=True)
-    
     dataset = load_dataset(dataset, name=remote_name, split='train', streaming=True)
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer, use_fast=False, legacy=False)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer, use_fast=False, legacy=False, token=hf_token)
     token_dtype = np.int32 if tokenizer.vocab_size > 2**16 else np.uint16
     nprocs = max(1, os.cpu_count() // 2)
     tokenize_fn = partial(tokenize_document, tokenizer=tokenizer, token_dtype=token_dtype)
@@ -81,6 +81,7 @@ if __name__ == "__main__":
     parser.add_argument('--remote_name', type=str, required=False, help='The name of the remote dataset to use.') # default="sample-10BT"
     parser.add_argument('--shard_size', type=int, default=10**8, help='The number of tokens per shard.')
     parser.add_argument('--tokenizer', type=str, default="facebook/MobileLLM-350M-layer-share", help='The name of the Hugging Face tokenizer to use.')
+    parser.add_argument('--hf-token', type=str, required=False, help='The Hugging Face token for authentication.')
     parser.add_argument('--output-dir', type=str, default="data/edu_fineweb10B", help='The directory to save the output shards.')
     args = parser.parse_args()
     main(**vars(args))
