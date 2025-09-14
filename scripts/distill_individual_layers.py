bc0ba1b6270f4595f9c2e615fa3cb735463a8af1
Gianluca Calò <gianluca.calo11@gmail.com>
2025-09-14 13:54:49 +0200

    fix dataloader duplication

--- scripts/distill_individual_layers.py
+++ scripts/distill_individual_layers.py
@@ -22,52 +22,8 @@ from attention_approximation.modeling_llama import LlamaForCausalLM as TeacherMo
 from attention_approximation.modeling_llama_approximated import LlamaApproximatedAttention
 from attention_approximation.pytorch import device_parse, intersect_dicts, WORLD_SIZE, LOCAL_RANK, RANK, rank_zero_only, de_parallel
 from attention_approximation.utils import LOGGER
+from attention_approximation.data import DistributedDataLoader
 import numpy as np
-
-
-def load_tokens(filename):
-    npt = np.load(filename).astype(np.int32)
-    ptt = torch.tensor(npt, dtype=torch.long)
-    return ptt
-
-
-class DistributedDataLoader:
-    def __init__(self, path: Path, batch_size: int, seq_len: int, split: str):
-        self.batch_size = batch_size
-        self.seq_len = seq_len
-        self.rank = max(RANK, 0)
-        self.num_processes = WORLD_SIZE
-        self.current_shard = None
-        assert split in {'train', 'val'}
-
-        # get the shard filenames
-        shards = sorted((s for s in path.glob("*.npy") if split in s.name), key=lambda x: x.name)
-        shards = [s.as_posix() for s in shards]
-        assert len(shards) > 0, f"no shards found for split {split}"
-        self.shards = shards
-        if LOCAL_RANK in {-1, 0}:
-            LOGGER.info(f"Found {len(shards)} shards for split {split}")
-        self.reset()
-
-    def reset(self):
-        if self.current_shard != 0:
-            self.current_shard = 0
-            self.tokens = load_tokens(self.shards[self.current_shard])
-        self.current_position = self.batch_size * self.seq_len * self.rank
-
-    def next_batch(self):
-        B, T = self.batch_size, self.seq_len
-        buf = self.tokens[self.current_position : self.current_position + B * T + 1]
-        x = (buf[:-1]).view(B, T) # inputs
-        y = (buf[1:]).view(B, T) # targets
-        # advance the position in the tensor
-        self.current_position += B * T * self.num_processes
-        # if loading the next batch would be out of bounds, advance to next shard
-        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
-            self.current_shard = (self.current_shard + 1) % len(self.shards)
-            self.tokens = load_tokens(self.shards[self.current_shard])
-            self.current_position = B * T * self.rank
-        return x, y
 
 
 DDP_ENABLED = WORLD_SIZE > 1
@@ -227,7 +183,7 @@ def setup_dataloaders(state):
         path=Path(state.config.data_path),
         batch_size=state.config.batch_size,
         seq_len=state.config.seq_length,
-        split='val'
+        split=state.config.val
     )
     return train_loader, val_loader
 
